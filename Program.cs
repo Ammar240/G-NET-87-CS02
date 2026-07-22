ac1a8f33935833303f4ae7e551f002f45354a9d2
Ammar <eammar240@gmail.com>
2026-07-22 03:37:27 +0300

    solve question 1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ internal class Program
     static void Main(string[] args)
     {
         #region question 1
-
+        int pages = 464;
+        Console.WriteLine(pages > 300? "Long Book" : "Short Book");
+        Console.WriteLine("==================");
         #endregion
+
     }
 }
