06e306bd1b06ef2ac4e90f60997abfe303ee78a0
Ammar <eammar240@gmail.com>
2026-07-22 04:01:55 +0300

    solve question 4

--- Program.cs
+++ Program.cs
@@ -37,10 +37,17 @@ internal class Program
 
         Console.WriteLine("==================");
         #endregion
+
         #region question 4
+        string[] books = {"Clean code", "The Pragmatic Programmer", "Refactoring"};
+        for(int i=0; i< books.Length; i++)
+        {
+            Console.WriteLine($"{i+1}. {books[i]}");
+        }
 
         Console.WriteLine("==================");
         #endregion
+
         #region question 5
 
         Console.WriteLine("==================");
