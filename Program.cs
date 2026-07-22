ee2d8042b655caf572a500c413034fdbb2a36a27
Ammar <eammar240@gmail.com>
2026-07-22 03:58:05 +0300

    solve question 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace session_03_Assingment;
+﻿using System.Globalization;
+using System.Runtime.Serialization.Json;
+
+namespace session_03_Assingment;
 
 internal class Program
 {
@@ -16,6 +19,51 @@ internal class Program
             Console.WriteLine("You can borrow this book");
         Console.WriteLine("==================");
 
+        #endregion
+
+        #region question 3
+        string title = "Refactoring";
+        switch (title)
+        {
+            case "Clean code":
+            case "Nice pick!":
+            case "Refactoring":
+                Console.WriteLine("Great choice");
+                break;
+            default:
+                Console.WriteLine("Never heard of it");
+                break;
+        }
+
+        Console.WriteLine("==================");
+        #endregion
+        #region question 4
+
+        Console.WriteLine("==================");
+        #endregion
+        #region question 5
+
+        Console.WriteLine("==================");
+        #endregion
+
+        #region question 6
+
+        Console.WriteLine("==================");
+        #endregion
+
+        #region question 7
+
+        Console.WriteLine("==================");
+        #endregion
+
+        #region question 8
+
+        Console.WriteLine("==================");
+        #endregion
+
+        #region question 9
+
+        Console.WriteLine("==================");
         #endregion
 
     }
