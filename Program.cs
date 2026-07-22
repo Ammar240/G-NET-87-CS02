cf5712c5cd22d551164bbf5f4e28a05eb7663463
Ammar <eammar240@gmail.com>
2026-07-22 03:48:16 +0300

    solve question 2

--- Program.cs
+++ Program.cs
@@ -4,10 +4,18 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        #region question 1
+        #region question 
         int pages = 464;
-        Console.WriteLine(pages > 300? "Long Book" : "Short Book");
+        Console.WriteLine(pages > 300 ? "Long Book" : "Short Book");
         Console.WriteLine("==================");
+        #endregion
+
+        #region queston 2
+        bool isAvailable = true;
+        if (pages > 300 && isAvailable)
+            Console.WriteLine("You can borrow this book");
+        Console.WriteLine("==================");
+
         #endregion
 
     }
